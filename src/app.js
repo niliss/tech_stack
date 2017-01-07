53eae6d3e0109d4f0df1e64d234848160d2ad478
niliss <niels@onrocks.co>
2017-01-07 21:12:37 +0100

    end of video number 92

--- src/app.js
+++ src/app.js
@@ -3,11 +3,17 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers/';
+import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
+
 
 const App = () => {
   return (
-    <Provider store="createStore(reducers)">
-    <View />
+    <Provider store={createStore(reducers)}>
+      <View style={{ flex: 1 }}>
+        <Header headerText = "Tech Stack" />
+        <LibraryList />
+      </View>
     </Provider>
   );
 };
